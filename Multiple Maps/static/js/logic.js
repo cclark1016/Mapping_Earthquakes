24f242ff1f45cf22404d162e478df6ac9e8937ad
Cristobal <chrisclark1016@gmail.com>
2020-10-11 11:38:27 -0400

    Multiple points

--- Multiple Maps/static/js/logic.js
+++ Multiple Maps/static/js/logic.js
@@ -1,38 +1,3 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-
-// Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-  onEachFeature: function(feature, layer){
-    console.log(layer);
-    layer.bindPopup("<h2>" + feature.properties.faa + "</h2>", "<h3>" + feature.properties.name + "</h3>");
-  }
-
-}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
